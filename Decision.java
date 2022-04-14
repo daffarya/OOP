19014716afe23ccf9c3c2406769df1fef442a6b8
daffarya <102981946+daffarya@users.noreply.github.com>
2022-04-14 21:02:03 +0700

    Add files via upload

---
+++ Decision.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.decision;
+
+/**
+ *
+ * @author daffa
+ */
+public class Decision {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 14;
+        if(a < b){
+            System.out.println("dasar payah dasar lemah");
+        }
+        else{
+            System.out.println("hebat");
+        }
+    }
+}
